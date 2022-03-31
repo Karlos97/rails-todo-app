a0755f880935d0a4aff161265f7e7adaa66d3398
Karlos97 <k.minta97@gmail.com>
2022-03-31 05:28:41 -0700

    adding basic functions for notebook

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,45 @@
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def create
+    @note = Note.new(params.require(:note).permit(:title, :body))
+    @note.save
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+
+    respond_to do |format|
+      format.html { redirect_to notes_path, notice: 'page was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def destroy_all
+    Note.delete_all
+
+    # respond_to do |format|
+    #   format.html { redirect_to notes_path, notice: 'page was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
+  end
+
+  def article_params
+    params.require(:note).permit(:title, :body)
+  end
+end
